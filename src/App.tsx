7b3e6cecc220885c58a1db7ebd17ee9114a3381b
k1nho <kinhong99@gmail.com>
2023-04-12 15:28:49 -0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Button, NoPropagationButton } from "./components/Button";
+import { LightSwitch } from "./components/Exercises";
 
 function App() {
     return (
-        <div className="App">
-            <div onClick={() => alert("parent activated")}>
-                <Button onSmash={() => alert("Smashhhh")}>Power</Button>
+        <div className="flex flex-col justify-center items-center space-y-8 min-h-screen bg-gray-700">
+            <div className="flex space-x-4" onClick={() => alert("parent activated")}>
+                <Button onSmash={() => alert("Smashhhh")}>Smash</Button>
                 <NoPropagationButton />
+            </div>
+            <div className="flex">
+                <LightSwitch />
             </div>
         </div>
     );
