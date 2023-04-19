e21476614f51dc497586fbbb50de0f013d149b67
k1nho <kinhong99@gmail.com>
2023-04-19 16:48:39 -0400

    state structure

--- src/components/Exercises.tsx
+++ src/components/Exercises.tsx
@@ -310,3 +310,87 @@ export const EditProfile = () => {
         </form>
     );
 }
+
+
+// Choosing the state structure
+// EX4)
+const letters = [{
+    id: 0,
+    subject: 'Ready for adventure?',
+    isStarred: true,
+}, {
+    id: 1,
+    subject: 'Time to check in!',
+    isStarred: false,
+}, {
+    id: 2,
+    subject: 'Festival Begins in Just SEVEN Days!',
+    isStarred: false,
+}];
+
+
+interface Letter {
+    letter: { id: number, subject: string, isStarred: boolean },
+    onToggle: (id: number) => void;
+    isSelected: boolean
+}
+
+
+export const MailClient = () => {
+    const [ids, setIds] = useState<number[]>([]);
+
+    // TODO: allow multiple selection
+    const selectedCount = ids.length;
+
+    function handleToggle(toggledId: number) {
+        if (!ids.includes(toggledId)) setIds([...ids, toggledId])
+        else setIds(ids.filter((id) => id !== toggledId))
+    }
+
+    return (
+        <>
+            <h2>Inbox</h2>
+            <ul>
+                {letters.map(letter => (
+                    <Letter
+                        key={letter.id}
+                        letter={letter}
+                        isSelected={
+                            // TODO: allow multiple selection
+                            ids.includes(letter.id)
+                        }
+                        onToggle={handleToggle}
+                    />
+                ))}
+                <hr />
+                <p>
+                    <b>
+                        You selected {selectedCount} letters
+                    </b>
+                </p>
+            </ul>
+        </>
+    );
+}
+
+
+export const Letter: React.FC<Letter> = ({ letter, onToggle, isSelected }) => {
+    {
+        return (
+            <li className={
+                isSelected ? 'selected' : ''
+            }>
+                <label>
+                    <input
+                        type="checkbox"
+                        checked={isSelected}
+                        onChange={() => {
+                            onToggle(letter.id);
+                        }}
+                    />
+                    {letter.subject}
+                </label>
+            </li>
+        )
+    }
+}
